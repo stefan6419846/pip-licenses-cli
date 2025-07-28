d9ee5845510bf8704d9cfafec71b13e91eee146f
stefan6419846 <96178532+stefan6419846@users.noreply.github.com>
2025-07-28 17:10:08 +0200

    add missing newline

--- piplicenses/errors.py
+++ piplicenses/errors.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2018 raimon
 # SPDX-FileCopyrightText: Copyright (c) 2025 stefan6419846
 
+
 class PipLicensesWarning(UserWarning):
     """
     Base class for warnings emitted by the pip-licenses-cli package.
